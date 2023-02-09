d083d5e8bf5a3dd68663a28dfba2e96c42dcf39d
StipGod <stipgod2@gmail.com>
2023-02-09 11:35:25 -0500

    last changes

--- movie/views.py
+++ movie/views.py
@@ -6,3 +6,6 @@ def home(request):
     #return HttpResponse('<h1>Welcome to Home Page</h1>')
     #return render(request,'home.html')
     return render(request,'home.html',{'name' : 'Esteban Bernal'})
+
+def about(request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
